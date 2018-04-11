11909ca6c14b971e39d1dbd06e05dd0e168e504f
gratitude <master@gratitudes-MacBook-Pro.local>
2018-04-11 11:10:51 -0700

    Refactor: basic buttons for deck

--- components/Deck.js
+++ components/Deck.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react'
 import {
   Text,
   View,
-  StyleSheet
+  StyleSheet,
+  Button
 } from 'react-native'
 
 class Deck extends Component {
@@ -17,7 +18,29 @@ class Deck extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.pageTitle}>Deck.js {`ID: ${this.props.navigation.state.params.deck.title}`}</Text>
+
+        <View style={styles.deckHeader}>
+          <Text style={styles.title}>
+            {`${this.props.navigation.state.params.deck.title}`}
+          </Text>
+          <Text style={styles.cardCount}>
+            {this.props.navigation.state.params.deck.cards ? `${this.props.navigation.state.params.deck.cards.length} cards` : `No cards available`}
+          </Text>
+        </View>
+
+        <Button
+          onPress={() => console.log("You added a Card")}
+          title="Add Card"
+          // color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+
+        <Button
+          onPress={() => console.log("Started the Quiz")}
+          title="Start Quiz"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
 
         <Text>
           {JSON.stringify(this.props.navigation.state.params.deck)}
@@ -37,6 +60,24 @@ const styles = StyleSheet.create({
   pageTitle: {
     textAlign: 'center',
     fontSize: 40,
+  },
+  deckHeader: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: 'lightgray',
+    minWidth: '90%',
+    maxHeight: 100,
+    marginTop: 10,
+    marginBottom: 10,
+    borderWidth: 3,
+    borderColor: 'black',
+  },
+  title: {
+    fontSize: 20,
+  },
+  cardCount: {
+    color: 'red',
   },
 })
 
