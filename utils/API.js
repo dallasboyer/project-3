699cdd71ad02f9dace815d0fb4aed70444f20fed
gratitude <master@gratitudes-MacBook-Pro.local>
2018-04-23 12:54:55 -0700

    Refactor: removed all but 1 from initial data dump

--- utils/API.js
+++ utils/API.js
@@ -23,57 +23,6 @@ const initData = {
         answer: 'The componentDidMount lifecycle event'
       }
     ]
-  },
-  Geography: {
-    title: 'Geography',
-  },
-  Farming: {
-    title: 'Farming',
-    cards: []
-  },
-  JavaScript: {
-    title: 'JavaScript',
-    cards: [
-      {
-        question: 'What is a closure',
-        answer: 'The combination of a function and the lexical environment within which that function was declared'
-      },
-      {
-        question: 'What does the spread operator look like',
-        answer: '...'
-      },
-    ]
-  },
-  Programming: {
-    title: 'Programming',
-    cards: [
-      {
-        question: 'What is the best programming language',
-        answer: 'Javascript is the best language'
-      }
-    ]
-  },
-  Tools: {
-    title: 'Tools',
-    cards: [
-      {
-        question: 'What is the most essential tool',
-        answer: 'axe'
-      },
-    ]
-  },
-  Vocabulary: {
-    title: 'Vocabulary',
-    cards: [
-      {
-        question: 'abhorrent',
-        answer: 'utterly opposed, or contrary, or in conflict (usually followed by to)'
-      },
-      {
-        question: 'magnanimous',
-        answer: 'generous in forgiving an insult or injury; free from petty resentfulness or vindictiveness'
-      },
-    ]
   }
 }
 
